d82759b73cea5d0ec666e702de661fc2fee705d6
YehWeiYang <uehweiyang@gmail.com>
2022-11-11 17:27:38 +0800

    建立maven  module

--- gitegg-common/src/main/java/org/weiyang/cloud/Main.java
+++ gitegg-common/src/main/java/org/weiyang/cloud/Main.java
@@ -2,6 +2,6 @@ package org.weiyang.cloud;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world  today is a good day!");
+        System.out.println("Hello world!");
     }
 }
