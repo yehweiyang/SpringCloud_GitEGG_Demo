4c9f8bc188fb68dafe82938202b54220151ce39d
yehweiyang <tw00182691@gmail.com>
2022-11-12 19:05:03 +0800

    SpringCloud微服務實戰——搭建企業級開發框架（六）

--- GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/weiyang/service/system/GitEggSystemApplication.java
+++ GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/weiyang/service/system/GitEggSystemApplication.java
@@ -3,12 +3,14 @@ package com.weiyang.service.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * gitegg-system 啟動類
  */
 @MapperScan("com.weiyang.*.*.mapper")
 @SpringBootApplication
+@ComponentScan(basePackages = "com.weiyang")
 public class GitEggSystemApplication {
 
     public static void main(String[] args) {
--- GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/weiyang/service/system/controller/SystemController.java
+++ GitEgg-Cloud/gitegg-service/gitegg-service-system/src/main/java/com/weiyang/service/system/controller/SystemController.java
@@ -1,6 +1,8 @@
 package com.weiyang.service.system.controller;
 
 import com.weiyang.service.system.service.ISystemService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "system")
 @AllArgsConstructor
+@Api(tags = "gitegg-system")
 public class SystemController {
 
     private final ISystemService systemService;
 
     @GetMapping(value = "list")
+    @ApiOperation(value = "system list介面")
     public Object list() {
         return systemService.list();
     }
 
 
     @GetMapping(value = "page")
+    @ApiOperation(value = "system page介面")
     public Object page() {
         return systemService.page();
     }
